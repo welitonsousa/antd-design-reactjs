ee31e20981113844ed3242ba353cc988a04f5e3e
Weliton de Sousa Araujo <welitonubuntu@gmail.com>
2021-01-13 18:27:28 -0300

    home page no layout

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Redirect, Route } from "react-router";
+import { LayoutPrivate } from "../containers/Layout/Layout";
+import "../containers/Layout/Layout.css";
 import { isLogged } from "../utils/auth";
 
 const PrivateRoute = (props) =>
   isLogged() ? (
-    <>
-      <div>
-        <h1>Layout</h1>
-        <Route {...props} />
-      </div>
-    </>
+    <LayoutPrivate className="body">
+      <Route {...props} />
+    </LayoutPrivate>
   ) : (
     <Redirect to="/login" />
   );
